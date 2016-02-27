6310ccaec19fa6141ebf64dd39e154013eedb53c
王仕晋 <wang@game.local>
2016-02-27 18:44:18 +0800

    add deeply sub command test

--- command_test.go
+++ command_test.go
@@ -12,7 +12,7 @@ func TestCommand(t *testing.T) {
 		Version string `cli:"v,version" usage:"show version" dft:"v0.0.0"`
 	}
 
-	app.Register(&Command{
+	sub1 := app.Register(&Command{
 		Name: "sub1",
 		Fn: func(ctx *Context) error {
 			if ctx.Path() != "sub1" {
@@ -27,11 +27,33 @@ func TestCommand(t *testing.T) {
 		ArgvFn: func() interface{} { return new(arg_t) },
 	})
 
-	err := app.Run([]string{
+	sub1.Register(&Command{
+		Name: "sub11",
+		Fn: func(ctx *Context) error {
+			if ctx.Path() != "sub1 sub11" {
+				t.Errorf("path: `%s` vs `%s`", ctx.Path(), "sub1 sub11")
+			}
+			argv := ctx.Argv().(*arg_t)
+			if argv.Help != false || argv.Version != "v1.0.0" {
+				t.Errorf("argv=%v", *argv)
+			}
+			return nil
+		},
+		ArgvFn: func() interface{} { return new(arg_t) },
+	})
+
+	if err := app.Run([]string{
 		"sub1",
 		"-h",
-	})
-	if err != nil {
-		t.Errorf("Run error: %v", err)
+	}); err != nil {
+		t.Errorf("Run `sub1` error: %v", err)
+	}
+
+	if err := app.Run([]string{
+		"sub1",
+		"sub11",
+		"--version=v1.0.0",
+	}); err != nil {
+		t.Errorf("Run `sub1 sub11` error: %v", err)
 	}
 }
