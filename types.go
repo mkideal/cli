95d31c52ef71dd68f78dd5187c56d0290dfb4a4d
mkideal <mkideal2@gmail.com>
2016-04-07 00:57:30 +0800

    add new builtin type

--- types.go
+++ types.go
@@ -1,10 +1,27 @@
 package cli
 
+import (
+	"fmt"
+)
+
 type Helper struct {
 	Help bool `cli:"!h,help" usage:"display help"`
 }
 
 type Addr struct {
-	Host string `cli:"host" usage:"remote host"`
-	Port uint16 `cli:"port" usage:"remote port" dft:"8080"`
+	Host string `cli:"host" usage:"specify host" dft:"0.0.0.0"`
+	Port uint16 `cli:"port" usage:"specify port" dft:"8080"`
+}
+
+type AddrWithShort struct {
+	Host string `cli:"H,host" usage:"specify host" dft:"0.0.0.0"`
+	Port uint16 `cli:"p,port" usage:"specify port" dft:"8080"`
+}
+
+func (addr Addr) String() string {
+	return fmt.Sprintf("%s:%d", addr.Host, addr.Port)
+}
+
+func (addr AddrWithShort) String() string {
+	return fmt.Sprintf("%s:%d", addr.Host, addr.Port)
 }
