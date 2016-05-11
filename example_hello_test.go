781403d470d003f4d03f0f2c91e96fc1f8d6e0af
mkideal <mkideal2@gmail.com>
2016-05-11 23:15:13 +0800

    fix hello example

--- example_hello_test.go
+++ example_hello_test.go
@@ -4,16 +4,16 @@ import (
 	"github.com/mkideal/cli"
 )
 
+type helloT struct {
+	cli.Helper
+	Name string `cli:"name" usage:"tell me your name" dft:"world"`
+	Age  uint8  `cli:"a,age" usage:"tell me your age" dft:"100"`
+}
+
 // This is a HelloWorld example
 func Example_hello() {
-	type argT struct {
-		cli.Helper
-		Name string `cli:"name" usage:"tell me your name" dft:"world"`
-		Age  uint8  `cli:"a,age" usage:"tell me your age" dft:"100"`
-	}
-
-	args := []string{"clitest", "--name=Cliper"}
-	cli.RunWithArgs(&argT{}, args, func(ctx *cli.Context) error {
+	args := []string{"app", "--name=Cliper"}
+	cli.RunWithArgs(new(helloT), args, func(ctx *cli.Context) error {
 		argv := ctx.Argv().(*argT)
 		ctx.String("Hello, %s! Your age is %d?\n", argv.Name, argv.Age)
 		return nil
