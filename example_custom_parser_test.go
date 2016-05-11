5de1ac62a58c853ead8de188563dc6a141e82a50
mkideal <mkideal2@gmail.com>
2016-05-11 23:06:07 +0800

    fix example files

--- example_custom_parser_test.go
+++ example_custom_parser_test.go
@@ -1,44 +1,10 @@
 package cli_test
 
 import (
-	"io/ioutil"
-	"os"
 	"reflect"
 
 	"github.com/mkideal/cli"
 )
-
-type Config1 struct {
-	A string
-	B int
-}
-
-type Config2 struct {
-	C string
-	D bool
-}
-
-// This example demonstrates how to use builtin praser(json,jsonfile)
-func Example_parser() {
-	type argT struct {
-		Cfg1 Config1 `cli:"cfg1" parser:"json"`
-		Cfg2 Config2 `cli:"cfg2" parser:"jsonfile"`
-	}
-	jsonfile := "1.json"
-	args := []string{"app",
-		`--cfg1`, `{"A": "hello", "B": 2}`,
-		`--cfg2`, jsonfile,
-	}
-	ioutil.WriteFile(jsonfile, []byte(`{"C": "world", "D": true}`), 0644)
-	defer os.Remove(jsonfile)
-
-	cli.RunWithArgs(new(argT), args, func(ctx *cli.Context) error {
-		ctx.JSON(ctx.Argv())
-		return nil
-	})
-	// Output:
-	// {"Cfg1":{"A":"hello","B":2},"Cfg2":{"C":"world","D":true}}
-}
 
 type myParser struct {
 	ptr interface{}
@@ -48,6 +14,7 @@ func newMyParser(ptr interface{}) cli.FlagParser {
 	return &myParser{ptr}
 }
 
+// Parse implements FlagParser.Parse interface
 func (parser *myParser) Parse(s string) error {
 	typ := reflect.TypeOf(parser.ptr)
 	val := reflect.ValueOf(parser.ptr)
@@ -70,7 +37,7 @@ func (parser *myParser) Parse(s string) error {
 	return nil
 }
 
-type Config3 struct {
+type config3 struct {
 	A int
 	B string
 }
@@ -81,7 +48,7 @@ func Example_customParser() {
 	cli.RegisterFlagParser("myparser", newMyParser)
 
 	type argT struct {
-		Cfg3 Config3 `cli:"cfg3" parser:"myparser"`
+		Cfg3 config3 `cli:"cfg3" parser:"myparser"`
 	}
 
 	args := []string{"app",
