7c1e9a70a4352a7243a6d70c8456386cfa82a25f
Dmitry Gorochovsky <dgoro@yahoo.com>
2020-07-20 09:40:24 +0300

    test

--- command.go
+++ command.go
@@ -194,6 +194,9 @@ func (cmd *Command) RunWith(args []string, writer io.Writer, resp http.ResponseW
 	}
 
 	if err != nil {
+		fmt.Printf("Color:%v\n", clr)
+		clr.Enable()
+		fmt.Println("suggestion", suggestion)
 		if cmd.OnRootPrepareError != nil {
 			err = cmd.OnRootPrepareError(err)
 		}
